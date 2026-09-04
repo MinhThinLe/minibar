340299cafd8c3cfc3d937fdf112d2da8935496ab
T0ast <minhthinle69@gmail.com>
2026-09-04 22:13:59 +0700

    Script: finished (not really) the script module

--- src/modules/script.rs
+++ src/modules/script.rs
@@ -1,6 +1,11 @@
 use std::{process::Command, thread::sleep};
 
-use iced::{futures::Stream, stream, widget::text};
+use iced::{
+    Background,
+    futures::Stream,
+    stream,
+    widget::{container, text},
+};
 use log::error;
 use minibar_derives::ModuleData;
 
@@ -11,14 +16,16 @@ use super::*;
 #[derive(ModuleData)]
 struct CommandOutput(String);
 
+#[derive(Clone)]
 struct ScriptConfig {
     command: Arc<str>,
-    name: Arc<str>,
+    poll_interval: Duration,
 }
 
 pub struct Script {
     config: ScriptConfig,
     output_buffer: String,
+    style: CommonStyle,
     id: [ModuleId; 1],
 }
 
@@ -36,7 +43,15 @@ impl Module for Script {
     }
 
     fn view(&self, _output: &Output) -> Element<'_, BarEvent> {
-        text!("{}", self.output_buffer.trim()).into()
+        container(text(&self.output_buffer))
+            .padding(self.style.padding)
+            .style(|_theme| container::Style {
+                text_color: self.style.foreground,
+                background: self.get_background(),
+                border: self.style.border,
+                ..Default::default()
+            })
+            .into()
     }
 
     fn subscription(&self) -> Option<Subscription<ModuleUpdate>> {
@@ -44,9 +59,11 @@ impl Module for Script {
             (
                 self.id[0],
                 self.config.command.clone(),
-                self.config.name.clone(),
+                self.config.poll_interval,
             ),
-            move |(destination, command, name)| worker(*destination, command.clone(), name.clone()),
+            move |(destination, command, poll_interval)| {
+                worker(*destination, *poll_interval, command.clone())
+            },
         ))
     }
 
@@ -56,47 +73,71 @@ impl Module for Script {
     {
         let id = [module_id_unique()];
 
+        let poll_interval = get_int(table, "poll_interval")
+            .map_or(DEFAULT_POLL_INTERVAL, |interval| {
+                Duration::from_millis(interval.cast_unsigned())
+            });
+
+        let command = get_str(table, "command").unwrap_or_default().into();
+
         let config = ScriptConfig {
-            name: "custom-module".into(),
-            command: "date".into(),
+            command,
+            poll_interval,
         };
+
+        let style = CommonStyle::from(table);
 
         Rc::new(Self {
             id,
             config,
+            style,
             output_buffer: String::new(),
         })
     }
 }
 
+impl Script {
+    fn get_background(&self) -> Option<Background> {
+        Some(Background::Color(self.style.background?))
+    }
+}
+
 fn worker(
     destination: ModuleId,
+    poll_interval: Duration,
     command: Arc<str>,
-    module_name: Arc<str>,
 ) -> impl Stream<Item = ModuleUpdate> {
     stream::channel(0, async move |mut output| {
-        let poll_interval = get_poll_interval(&module_name);
-
         let mut subprocess = Command::new("sh");
         let command = subprocess.arg("-c").arg(command.as_ref());
+
         loop {
-            let result = match command.output() {
-                Ok(result) => String::from_utf8(result.stdout),
-                Err(err) => {
-                    error!("{err}");
-                    return;
-                }
+            let Some(result) = unwrap_output(command.output()) else {
+                return;
             };
-            let content = match result {
-                Ok(result) => CommandOutput(result),
-                Err(utf8_err) => {
-                    error!("{utf8_err}");
-                    return;
-                }
-            };
+
+            let content = CommandOutput(result);
             send_data(&mut output, destination, content).await;
 
             sleep(poll_interval);
         }
     })
+}
+
+fn unwrap_output(command_output: std::io::Result<std::process::Output>) -> Option<String> {
+    match command_output {
+        Ok(result) => {
+            if !result.status.success() {
+                error!("{}", String::from_utf8_lossy(&result.stderr).trim());
+                return None;
+            }
+            String::from_utf8(result.stdout)
+                .map(|res| res.trim().to_string())
+                .ok()
+        }
+        Err(err) => {
+            error!("{err}");
+            None
+        }
+    }
 }
